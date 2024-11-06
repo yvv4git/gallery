354619f0ce7614364d150c926191ca34b88be1ba
yvv4git <yvv4m@mail.ru>
2024-11-06 22:37:36 +0300

    Footer at the bottom

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,20 @@ export default {
 </script>
 
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex: 1;
 }
 </style>
